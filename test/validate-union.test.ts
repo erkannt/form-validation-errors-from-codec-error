96f7fa559078c6cecc2f03aae3d9b97de56bf25c
Daniel Haarhoff <daniel@rknt.de>
2023-04-05 12:30:28 +0100

    Failing case of no element of union matching

--- test/validate-union.test.ts
+++ test/validate-union.test.ts
@@ -1,16 +1,19 @@
-import * as E from 'fp-ts/Either';
-import { pipe } from 'fp-ts/lib/function';
-import * as t from 'io-ts';
-import * as tt from 'io-ts-types';
+import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/lib/function";
+import * as t from "io-ts";
+import * as tt from "io-ts-types";
+import { groupByKey } from "../src";
 
 const ConflictOfInterestC = t.union([
-	t.type({hasConflictOfInterest: t.literal('no')}),
-	t.type({hasConflictOfInterest: t.literal('yes'), conflictOfInterestDetails: tt.NonEmptyString}),
-]
-)
+  t.type({ hasConflictOfInterest: t.literal("no") }),
+  t.type({
+    hasConflictOfInterest: t.literal("yes"),
+    conflictOfInterestDetails: tt.NonEmptyString,
+  }),
+]);
 
-describe('report bad parts of union in human friendly way', () => {
-	describe('given a valid body', () => {
+describe("report bad parts of union in human friendly way", () => {
+  describe("given a valid body", () => {
     const body = {
       hasConflictOfInterest: "no",
     };
@@ -20,5 +23,16 @@ describe('report bad parts of union in human friendly way', () => {
     it("returns form on right", () => {
       expect(form).toStrictEqual(E.right(body));
     });
-	})
-})
+  });
+
+  describe("given no member of the union is valid", () => {
+    const body = {
+      hasConflictOfInterest: undefined,
+    };
+
+    const form = pipe(body, ConflictOfInterestC.decode, E.mapLeft(groupByKey));
+    it.failing("returns something useful on the left", () => {
+      expect(form).toStrictEqual(E.left("whoops"));
+    });
+  });
+});
