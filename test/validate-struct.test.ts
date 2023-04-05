f806f943ade71216d46ddc39aba8361dd970fa2f
Daniel Haarhoff <daniel@rknt.de>
2023-04-05 11:44:00 +0100

    Fail in human friendlier way thanks to io-ts-reporters

--- test/validate-struct.test.ts
+++ test/validate-struct.test.ts
@@ -1,10 +1,14 @@
 import * as E from "fp-ts/Either";
 import { pipe } from "fp-ts/lib/function";
+import { ValidationError } from "io-ts";
+import { formatValidationErrors } from "io-ts-reporters";
 import * as C from "io-ts/Codec";
+import { DecodeError } from "io-ts/lib/Decoder";
+import * as t from "io-ts";
 
-const FormC = C.struct({
-  foo: C.string,
-  bar: C.number,
+const FormC = t.type({
+  foo: t.string,
+  bar: t.number,
 });
 
 describe("report bad parts of struct in human friendly way", () => {
@@ -26,7 +30,7 @@ describe("report bad parts of struct in human friendly way", () => {
       bar: 42,
     };
 
-    const form = pipe(body, FormC.decode);
+    const form = pipe(body, FormC.decode, E.mapLeft(formatValidationErrors));
 
     it.failing("return a human friendly prompt as part of left", () => {
       expect(form).toStrictEqual(E.left("Please provide a string for `foo`"));
